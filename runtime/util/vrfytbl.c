dfaaeb5e29299d8f2bba456c5b05754d08864422
Mohammad Ali Nikseresht <anikser@ibm.com>
2018-07-26 11:47:49 -0400

    Add barebones static bytecode verification for withfield, defaultvalue
    This change adds basic functionality to the bytecode verifier to handle
    defaultvalue and withfield bytecodes.
    
    Signed-off-by: Mohammad Ali Nikseresht <anikser@ibm.com>

--- runtime/util/vrfytbl.c
+++ runtime/util/vrfytbl.c
@@ -227,8 +227,8 @@ const U_16 J9JavaBytecodeVerificationTable[] = {
 		0x1700 /* JBunimplemented - 201 */,
 		0x1700 /* JBbreakpoint - 202 */,
 #if defined(J9VM_OPT_VALHALLA_VALUE_TYPES)
-		0x0000 /* JBdefaultvalue = 203 */,
-		0x0000 /* JBwithfield = 204 */,
+		0x1200 /* JBdefaultvalue = 203 */,
+		0x1000 /* JBwithfield = 204 */,
 #else /* defined(J9VM_OPT_VALHALLA_VALUE_TYPES) */
 		0x1700 /* JBunimplemented - 203 */,
 		0x1700 /* JBunimplemented - 204 */,
