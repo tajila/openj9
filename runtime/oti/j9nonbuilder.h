887f680e74be11f101734086d617eec288bfca28
Theresa Mammarella <tmammare@redhat.com>
2020-12-08 15:18:32 -0800

    Snapshot+restore lock persistence & multithreading
    [skip ci]
    
    Signed-off-by: Theresa Mammarella <tmammare@redhat.com>

--- runtime/oti/j9nonbuilder.h
+++ runtime/oti/j9nonbuilder.h
@@ -5037,6 +5037,8 @@ typedef struct J9VMThread {
 #if defined(J9VM_OPT_SNAPSHOTS)
 	/* BP for the native special frame used when restoring a Java app thread */
 	UDATA *restoreThreadBP;
+	J9VMEntryLocalStorage restoreEls;
+	UDATA waitingMonitorFixupId;
 #endif /* defined(J9VM_OPT_SNAPSHOTS) */
 } J9VMThread;
 
